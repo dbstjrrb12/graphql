5743a3e20db82b445c24c78f7f56603bbb51640b
yoonseokgyu <dbstjrrb1@gmail.com>
2023-07-03 22:48:24 +0900

    [fix] : Mutation 타입 및 resolver 선언
    - REST API 에서 POST,PUT,DELETE 와 같이 서버에 변경을 발생시키는 http 메소드와 동일한 역할
    - 해당 GraphQL API를 실행할 때, 값을 전달 받을 수 있는데 이 값은 resolver의 두 번쩨 인자로 전달됨

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 /** DB와 같은 역할 */
-const tweets = [{
-  id : '1', 
-  text : 'first one', 
-  userId : '2'
-}, 
-{
-  id : '1', 
-  text : 'first one', 
-  userId : '1'
-}]
+const tweets = [
+  {
+    id: '1',
+    text: 'first one',
+    userId: '2',
+  },
+  {
+    id: '1',
+    text: 'first one',
+    userId: '1',
+  },
+];
 /** ------------ */
 
 const typeDefs = gql`
@@ -27,15 +29,30 @@ const typeDefs = gql`
   type Query {
     allTweet: [Tweet]
   }
+  type Mutation {
+    postTweet(userId: ID, text: String): Tweet
+  }
 `;
 
 const resolvers = {
-  Query {
-    allTweet() {
+  Query: {
+    allTweets() {
       return tweets;
-    }
-  }
-}
+    },
+  },
+  Mutation: {
+    postTweet(root, { userId, text }) {
+      const newTweet = {
+        userId,
+        text,
+        id: tweets.length + 1,
+      };
+
+      tweets.push(newTweet);
+      return newTweet;
+    },
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
