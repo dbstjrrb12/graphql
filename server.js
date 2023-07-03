38b4f1b69f14edc6516370895affea581d7b5b36
yoonseokgyu <dbstjrrb1@gmail.com>
2023-07-03 22:29:51 +0900

    [fix] : typeDefs 선언
    - GraphQL 서버 실행을 위해서, 데이터의 형태(shape)을 미리 정의해야 한다.
    - 타입 선언에서 Query 타입은 필수적으로 선언되어야 한다.
    - Query 타입에 선언된 필드값은 REST API 에서 GET으로 가져올 수 있는 것과 동일하다

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import { ApolloServer, gql } from 'apollo-server';
 
-const server = new ApolloServer({});
+const typeDefs = gql`
+  type User {
+    id: ID
+    firstname: String
+    lastname: String
+  }
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+  type Query {
+    allTweet: [Tweet]
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
